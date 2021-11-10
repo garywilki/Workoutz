44ca276017b461105978bfc403d25ec1e217cec6
garywilki <wil20223@byui.edu>
2021-11-10 13:39:51 -0800

    Commit test after revert.

--- app/src/main/java/com/example/workoutz/MainActivity.java
+++ app/src/main/java/com/example/workoutz/MainActivity.java
@@ -94,4 +94,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewProfile.class);
         startActivity(intent);
     }
+
+    public void doNothingTest() {
+        int j = 1;
+    }
 }
