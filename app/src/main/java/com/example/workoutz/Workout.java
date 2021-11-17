10133df3e2f98b567bc52cd04f5a65f9f393cc1a
Dallin Suggs <dallin.suggs@gmail.com>
2021-11-17 10:02:41 -0700

    Fully implement profile timer functionality, with updating UI
    Made it so the workout activity shows work/rest interval timers playing in succession, still pausable and startable as before.

--- app/src/main/java/com/example/workoutz/Workout.java
+++ app/src/main/java/com/example/workoutz/Workout.java
@@ -2,7 +2,9 @@ package com.example.workoutz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,11 +17,15 @@ public class Workout extends AppCompatActivity {
     // Timer timer;
 
     TextView time;
+    TextView repsRemaining;
+    TextView currentTimer;
     Button pauseButton;
     String timeString;
     WorkoutPresenter presenter;
     Boolean running;
     Boolean finished;
+    int profileID;
+    Profile p;
 
 
     @Override
@@ -27,15 +33,28 @@ public class Workout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
+        // Obtain the profile ID
+        Intent intent = getIntent();
+        profileID = intent.getIntExtra(MainModel.EXTRA_INT_PROFILEID, Profile.NULL_ID);
+        p = MainModel.getProfile(profileID);
+
         running = false;
         finished = false;
 
         time = findViewById(R.id.timer_text);
+        repsRemaining = findViewById(R.id.reps_remaining);
+        currentTimer = findViewById(R.id.current_timer);
+
+        time.setText("0");
+
+        repsRemaining.setText("5");
+
+        currentTimer.setText("Rest");
 
         pauseButton = findViewById(R.id.pause_button);
 
         // Set up the presenter so it has a reference to this activity, as well as the current time
-        presenter = new WorkoutPresenter(this, timeString);
+        presenter = new WorkoutPresenter(this, p);
     }
 
     public void togglePauseButton(View view) {
@@ -66,15 +85,34 @@ public class Workout extends AppCompatActivity {
         // Returns to ProfileDashboard activity
     }
 
-    public void updateTime(String newTime, Boolean finished) {
+    public void updateTime(String newTime, Boolean finished, int reps, String workState) {
         // Updates the visible time on the screen
-        time.setText(newTime);
+        if (newTime != null) {
+            time.setText(newTime);
+        }
         this.finished = finished;
+
+        currentTimer.setText(workState);
+
+        repsRemaining.setText(String.valueOf(reps));
 
         if (finished == true) {
             ViewGroup layout = (ViewGroup) pauseButton.getParent();
             if(null!=layout)
                 layout.removeView(pauseButton);
+                currentTimer.setText("");
+        }
+    }
+
+    public void backButton(View view) {
+        if (p != null) {
+            Log.i("MainActivity", "MainActivity.goToProfileDashboard() -> " + p.id + " " + p.name + " " + p.reps + " " + p.workIntervalSeconds + " " + p.restIntervalSeconds + " & " + p.nextID);
+            Intent intent = new Intent(this, ProfileDashboard.class);
+            intent.putExtra(MainModel.EXTRA_INT_PROFILEID, p.id);
+            startActivity(intent);
+        }
+        else {
+            Log.e("MainActivity", "MainActivity.goToProfileDashboard() -> NULL Profile reference");
         }
     }
 }
--- app/src/main/java/com/example/workoutz/WorkoutPresenter.java
+++ app/src/main/java/com/example/workoutz/WorkoutPresenter.java
@@ -10,12 +10,24 @@ public class WorkoutPresenter {
     private CountDownTimer timer;
     public int currentMillis;
 
+    //initialize profile interval and rep variables
+    int pReps;
+    int pWork;
+    int pRest;
+    Boolean working = false;
+    Boolean preWorkout = true;
 
-    public WorkoutPresenter(Workout activity, String timeString) {
+
+    public WorkoutPresenter(Workout activity, Profile p) {
 
         // Initialize the time and activity variables
         this.activity = activity;
-        this.currentMillis = 610000;
+        this.currentMillis = 4000;
+
+        //set profile variables for workout, from profile data
+        pReps = p.reps;
+        pWork = p.workIntervalSeconds * 1000;
+        pRest = p.restIntervalSeconds * 1000;
 
         calculateTime(currentMillis);
 
@@ -32,7 +44,43 @@ public class WorkoutPresenter {
             }
 
             public void onFinish() {
-                activity.updateTime("Finished!", true);
+                // If reps goes down to one, finish the workout. Otherwise, start a new timer with the correct values
+                if (pReps <= 1) {
+                    working = false;
+                    pReps -= 1;
+                    activity.updateTime("Finished!", true, pReps, "Rest");
+                } else {
+                    if (preWorkout) {
+
+                        // Make sure the pre-workout prep time ends by setting preWorkout to false
+                        preWorkout = false;
+
+                        // Move into the first Work section
+                        working = true;
+
+                        // Update the timer currentMillis value to have the correct work interval value
+                        currentMillis = pWork;
+                        activity.updateTime(null, false, pReps, "Rest");
+
+                        // Use recursion to start the next timer in the chain, if the current timer finishes
+                        startTimer(view);
+                    } else {
+                        if (!working) {
+
+                            // Follow same update procedure as above every time we switch from Work to Rest and vice versa
+                            working = true;
+                            pReps -= 1;
+                            currentMillis = pWork;
+                            activity.updateTime(null, false, pReps, "Work");
+                            startTimer(view);
+                        } else if (working) {
+                            working = false;
+                            currentMillis = pRest;
+                            activity.updateTime(null, false, pReps, "Rest");
+                            startTimer(view);
+                        }
+                    }
+                }
             }
 
         }.start();
@@ -43,11 +91,13 @@ public class WorkoutPresenter {
     }
 
     public void calculateTime(int millis) {
+
+        // This calculates the time and puts it in a displayable format for the UI to be updated
         String newTime;
 
         int totalSeconds = (millis / 1000);
         int minutes = totalSeconds / 60;
-        int seconds = totalSeconds % 60;
+        int seconds = totalSeconds % 60 + 1;
 
         if (seconds < 10) {
             newTime = minutes + ":0" + seconds;
@@ -55,6 +105,20 @@ public class WorkoutPresenter {
             newTime = minutes + ":" + seconds;
         }
 
-        activity.updateTime(newTime, false);
+        // Adjust the current work state based on appropriate boolean values
+
+        String workState;
+
+        if (preWorkout) {
+            workState = "Get ready!";
+        } else if (working) {
+            workState = "Work";
+        } else {
+            workState = "Rest";
+        }
+
+        // Send everything to the Workout Activity to update what's displayed
+
+        activity.updateTime(newTime, false, pReps, workState);
     }
 }
